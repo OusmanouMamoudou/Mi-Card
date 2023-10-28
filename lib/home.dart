1fd06042763d66ca98f7721075cc175fa5c82fae
OusmanouMamoudou <ous3310@gmail.com>
2023-10-28 07:55:24 +0100

    xyz

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// Import the Font Awesome Flutter package for icons.
+// Importation du package Font Awesome Flutter pour les icônes.
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,18 +9,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      // Create a Scaffold, a basic framework for the app's UI.
+      // Crée un Scaffold, un cadre de base pour l'interface utilisateur de l'application.
+
+      backgroundColor:
+          Colors.teal, // Set the background color to teal (green-blue).
+      // Définit la couleur d'arrière-plan en teal (vert-bleu).
+
       body: SafeArea(
+        // Create a safe area for the app's content, avoiding potential clipping on the screen edges.
+        // Crée une zone sécurisée pour le contenu de l'application, évitant les zones de découpe potentielles sur les bords de l'écran.
+
         child: Column(
+          // Create a column to organize content in vertical columns.
+          // Crée une colonne pour organiser les elements en colonnes verticales.
           children: [
             const SizedBox(
+              // Add a fixed-size vertical space (height of 5 pixels).
+              // Ajoute un espace vertical de taille fixe (hauteur de 5 pixels).
               height: 5,
             ),
             const Material(
-              elevation: 5,
-              color: Colors.white,
-              shadowColor: Colors.black,
+              // Create a Material with shadows and rounded corners.
+              // Crée un matériau (conteneur) avec des ombres et des coins arrondis.
+
+              elevation: 5, // Définit l'élévation du matériau.
+              color: Colors.white, // Set the  color of the Material to white.
+              // Définit la couleur du matériau en blanc.
+              shadowColor: Colors
+                  .black, // Set the shadow color of the Material to black.
+              // Définit la couleur de l'ombre en noir.
               borderRadius: BorderRadius.only(
+                // Set rounded top corners for the Material.
+                // Définit les coins arrondis supérieurs du matériau.
                 topLeft: Radius.circular(
                   50,
                 ),
@@ -27,15 +50,23 @@ class Home extends StatelessWidget {
                 ),
               ),
               child: Padding(
+                // Add padding around the text.
+                // Ajoute un espace de rembourrage autour du texte.
                 padding: EdgeInsets.symmetric(
                   horizontal: 45,
                   vertical: 10,
                 ),
                 child: Text(
-                  "CONTACT",
+                  "CONTACT", // Display the text "CONTACT."
+                  // Affiche le texte "CONTACT".
                   style: TextStyle(
-                    fontSize: 50,
-                    fontFamily: "Pixel",
+                    // Define the style of the text.
+                    // Définit le style du texte.
+                    fontSize: 50, // Set the font size.
+                    // Définit la taille de la police.
+                    fontFamily:
+                        "Pixel", // Set the font family. Added in pubspec.yaml file
+                    // Définit la police de caractères. Ajouté dans le fichier pubspec.yaml
                     color: Colors.teal,
                   ),
                 ),
@@ -53,7 +84,9 @@ class Home extends StatelessWidget {
                 padding: EdgeInsets.all(10),
                 child: CircleAvatar(
                   radius: 80,
-                  backgroundImage: AssetImage("images/ous.jpg"),
+                  backgroundImage: AssetImage(
+                      "images/ous.jpg"), // Set the background image of the circle.
+                  // Définit l'image de fond du cercle.
                 ),
               ),
             ),
@@ -62,36 +95,22 @@ class Home extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
+                letterSpacing: 1.5, // Set letter spacing.
+                // Définit l'espacement des lettres.
                 fontSize: 30,
                 fontFamily: "Pacifico",
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Software Engineer | Web Developer ",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal.shade100,
-                    fontSize: 20,
-                    fontFamily: "Pixel",
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "Flutter Developer",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal.shade100,
-                    fontSize: 20,
-                    fontFamily: "Pixel",
-                  ),
-                ),
-              ],
+            Text(
+              "Software Engineer | Web Developer \n " // \n To break line. \n Pour sauter la ligne
+              "Flutter Developer",
+              textAlign: TextAlign.center, // Center the text. Centre le texte.
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.teal.shade100,
+                fontSize: 20,
+                fontFamily: "Pixel",
+              ),
             ),
             const SizedBox(
               height: 15,
@@ -101,11 +120,14 @@ class Home extends StatelessWidget {
                 elevation: 20,
                 child: ListTile(
                   leading: FaIcon(
-                    FontAwesomeIcons.phone,
+                    // Add an icon at the beginning of the list from Fontawesome
+                    // Ajoute une icône en tête de liste à patir de FontAwesome
+                    FontAwesomeIcons
+                        .phone, // Use phone icon. Utilise l'icône de téléphone.
                     color: Colors.teal,
                   ),
                   title: Text(
-                    "+237690131336",
+                    "+237652384805",
                     style: TextStyle(
                         color: Colors.teal,
                         fontWeight: FontWeight.bold,
@@ -113,15 +135,21 @@ class Home extends StatelessWidget {
                   ),
                 )),
             const Card(
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                //Create a card to dislay contact infos
+                // Crée une autre carte pour afficher une adresse e-mail.
+                margin: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical:
+                        10), // Set card margins. Définit les marges de la carte.
                 elevation: 20,
                 child: ListTile(
                   leading: FaIcon(
-                    FontAwesomeIcons.message,
+                    FontAwesomeIcons
+                        .message, // Use of message Icon. Utilise l'icône de message.
                     color: Colors.teal,
                   ),
                   title: Text(
-                    "ous3310@gmail.com",
+                    "ousmamdev@gmail.com",
                     style: TextStyle(
                         color: Colors.teal,
                         fontWeight: FontWeight.bold,
@@ -136,7 +164,8 @@ class Home extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
+                fontStyle: FontStyle.italic, // Set the font style to italic.
+                // Définit le style de la police en italique.
                 fontSize: 20,
                 fontFamily: "Pixel",
               ),
@@ -145,14 +174,19 @@ class Home extends StatelessWidget {
               height: 10,
             ),
             const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              // Create a row to display social media icons.
+              // Crée une rangée pour afficher les icônes des médias sociaux.
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Center the icons horizontally.
+              // Centre les icônes horizontalement.
               children: [
                 FaIcon(
                   FontAwesomeIcons.github,
                   color: Colors.white,
                   size: 40,
                 ),
-                SizedBox(width: 10),
+                SizedBox(width: 10), // Add  horizontal spacing.
+                // Ajoute un espace horizontal.
                 FaIcon(
                   FontAwesomeIcons.facebook,
                   color: Colors.white,
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:mi_card/home.dart';
+import 'package:flutter/material.dart'; 
+// Import the Flutter package for UI widgets and features.
+// Importation du package Flutter pour les widgets et les fonctionnalités de l'interface utilisateur.
+import 'package:mi_card/home.dart'; 
+// Import the home.dart file (could be another file in your project).
+// Importation du fichier home.dart (peut être un autre fichier de votre projet).
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      const MyApp()); 
+      // Entry point of the application, calling runApp to run the app.
+      // Point d'entrée de l'application, appel de la fonction runApp pour exécuter l'application.
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Define a class MyApp that extends StatelessWidget, indicating it's an immutable widget.
+  // Définition d'une classe MyApp qui étend StatelessWidget, ce qui signifie que c'est un widget immuable.
+  const MyApp({super.key}); 
+  // Constructor for the MyApp class.
+ // Constructeur de la classe MyApp.
 
   @override
   Widget build(BuildContext context) {
+    // The build method that defines the app's appearance.
+     // Méthode build qui définit l'apparence de l'application.
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+      // Return a MaterialApp widget, which is the root widget of the app.
+       // Retourne un widget MaterialApp, qui est le widget racine de l'application.
+
+      debugShowCheckedModeBanner:false, 
+          // Disable the display of the debug banner in development mode.
+           // Désactive l'affichage du bannière de débogage en mode développement.
+
+      home: Home(), // Set the Home widget as the home screen of the app.
+       // Définit le widget Home comme écran d'accueil de l'application.
     );
   }
 }
